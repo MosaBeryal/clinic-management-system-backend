4b3adc98459f3cba1a6fe0ee8ac26cc76eddcad9
mosa beryal <beryalmosa@gmail.com>
2024-05-28 11:11:31 +0500

    get all patient route and controller

--- controllers/patientController.js
+++ controllers/patientController.js
@@ -1,69 +1,83 @@
 const { Patient } = require("../models");
 
-// Controller for adding a new patient
-exports.addPatient = async (req, res) => {
+exports.getAllPatients = async (req, res) => {
     try {
-
-        const {
-            patientId,
-            patientName,
-            gender,
-            birthday,
-            issuancePolicyNumber,
-            address,
-            phoneNumber,
-            email,
-        } = req.body;
-
-        //find if there is existing user 
-        const existingPatient = await Patient.findOne({
-            where: { patientId: patientId }
-        });
-
-        if (existingPatient) {
-            return res.status(400).json({ message: "Patient already exists" });
+        const patients = await Patient.findAll();
+        if (!patients) {
+            return res.status(404).json({ message: "No patients found" });
         }
-
-        const newPatient = await Patient.create({
-            patientId,
-            patientName,
-            gender,
-            birthday,
-            issuancePolicyNumber,
-            address,
-            phoneNumber,
-            email,
-        });
-        res
-            .status(201)
-            .json({ message: "Patient added successfully", patient: newPatient });
+        res.status(200).json(patients);
     } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server error");
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
     }
-};
+}
 
-// Controller for deleting a patient by patientId
-exports.deletePatient = async (req, res) => {
-    try {
-        const { patientId } = req.params;
 
-        if (!patientId) {
-            return res.status(400).json({ message: "Patient ID is required" });
+    // Controller for adding a new patient
+    exports.addPatient = async (req, res) => {
+        try {
+
+            const {
+                patientId,
+                patientName,
+                gender,
+                birthday,
+                issuancePolicyNumber,
+                address,
+                phoneNumber,
+                email,
+            } = req.body;
+
+            //find if there is existing user 
+            const existingPatient = await Patient.findOne({
+                where: { patientId: patientId }
+            });
+
+            if (existingPatient) {
+                return res.status(400).json({ message: "Patient already exists" });
+            }
+
+            const newPatient = await Patient.create({
+                patientId,
+                patientName,
+                gender,
+                birthday,
+                issuancePolicyNumber,
+                address,
+                phoneNumber,
+                email,
+            });
+            res
+                .status(201)
+                .json({ message: "Patient added successfully", patient: newPatient });
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Server error");
         }
+    };
 
-        const patient = await Patient.findOne({
-            where: {
-                patientId: patientId,
-            },
-        });
-        if (!patient) {
-            return res.status(404).json({ msg: "Patient not found" });
+    // Controller for deleting a patient by patientId
+    exports.deletePatient = async (req, res) => {
+        try {
+            const { patientId } = req.params;
+
+            if (!patientId) {
+                return res.status(400).json({ message: "Patient ID is required" });
+            }
+
+            const patient = await Patient.findOne({
+                where: {
+                    patientId: patientId,
+                },
+            });
+            if (!patient) {
+                return res.status(404).json({ msg: "Patient not found" });
+            }
+            await patient.destroy();
+            res.status(200).json({ msg: "Patient deleted" });
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Server error");
         }
-        await patient.destroy();
-        res.status(200).json({ msg: "Patient deleted" });
-    } catch (error) {
-        console.error(error.message);
-        res.status(500).send("Server error");
-    }
-};
+    };
--- routes/patientRoutes.js
+++ routes/patientRoutes.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 const patientController = require('../controllers/patientController');
 
+//get patients
+router.get('/', patientController.getAllPatients);
+
+
 // Route for adding a new patient
 router.post('/add', patientController.addPatient);
+
+
 
 // Route for deleting a patient by patientId
 router.delete('/:patientId', patientController.deletePatient);
