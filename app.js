ed764733ee517766cfd19a8848119cf295adc6f9
mosa beryal <beryalmosa@gmail.com>
2024-05-29 20:50:57 +0500

    Add CORS middleware

--- app.js
+++ app.js
@@ -12,13 +12,12 @@ const imagingExamRoutes = require("./routes/imagingExamRoutes");
 const medicalBillingRoutes = require("./routes/medicalBillingRoutes");
 const authenticateToken = require('./middleware/auth');
 
-// Middleware to parse URL-encoded request bodies
 app.use(express.urlencoded({ extended: true }));
 
-// Middleware to parse JSON request bodies
+app.use(cors())
+
 app.use(express.json());
 
-// Add your routes here
 app.use('/api/auth/', authRoutes);
 app.use('/api/patient/', patientRoutes);
 app.use('/api/consultation/', consultationRoutes);
@@ -27,8 +26,7 @@ app.use('/api/imaging-exam/', imagingExamRoutes)
 app.use('/api/medical-billing/', medicalBillingRoutes)
 
 
-// Sync models with the database
-sequelize.sync({ alter: true }) // alter: true will update the schema without dropping tables
+sequelize.sync({ alter: true })
     .then(() => {
         console.log('Database & tables synchronized!');
     })
