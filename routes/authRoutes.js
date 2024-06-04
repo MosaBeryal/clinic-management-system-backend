315ad3dc4e8ad99adf43d5613aa5efd5889d7201
mosa beryal <beryalmosa@gmail.com>
2024-06-04 16:43:32 +0500

    Add getAllUser route in authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,6 @@ const authController = require("../controllers/authController");
 
 router.post("/signin", authController.signIn);
 router.post("/signup", authController.signUp);
+router.get("/users", authController.getAllUser);
 router.patch("/user-update/:userId", authController.updateUser);
 module.exports = router;
